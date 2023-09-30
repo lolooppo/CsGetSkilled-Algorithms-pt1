f8d6fd0885aa27b851c63106a756246741fc5788
Alaa_Omran <99070234+lolooppo@users.noreply.github.com>
2023-09-30 20:41:20 +0300

    Update _binary_search_.cpp

--- _binary_search_.cpp
+++ _binary_search_.cpp
@@ -100,4 +100,62 @@ int minDays(vector<int>& bloomDay, int m, int k) {
     }
     return res;
 }
-//
+//https://leetcode.com/problems/heaters/description
+bool is(vector<int>& ho, vector<int>& he,int m){
+    int j = 0;
+    for(int i = 0;i<he.size();i++){
+        int left = he[i]-m , right = he[i]+m;
+        while(j<ho.size() and ho[j]>=left and ho[j]<=right){
+            j++;
+        }
+    }
+    return j==ho.size();
+}
+int findRadius(vector<int>& ho, vector<int>& he) {
+    sort(he.begin(),he.end());
+    sort(ho.begin(),ho.end());
+    int l = 0 , r = 1e9,res = 0;
+    while(l<=r){
+        int mid = l + (r-l)/2;
+        if(is(ho,he,mid)){
+            r = mid-1;
+            res = mid;
+        }else{
+            l = mid+1;
+        }
+    }
+    return res;
+}
+//https://leetcode.com/problems/most-profit-assigning-work/submissions/1051741534/
+int maxProfitAssignment(vector<int>& d, vector<int>& p, vector<int>& w){
+    vector<pair<int,int>>vp;
+    for(int i =0 ;i<(int)d.size();i++){
+        vp.push_back(make_pair(d[i],p[i]));
+    }
+    sort(d.begin(),d.end()),sort(vp.begin(),vp.end());
+    int res = 0;
+    for(int i = 1;i<(int)vp.size();i++){
+        vp[i].second = max(vp[i].second,vp[i-1].second);
+    }
+    for(int i = 0;i<(int)w.size();i++){
+        int idx = upper_bound(d.begin(),d.end(),w[i]) - d.begin();
+        if(idx){
+            idx--;
+            res+=vp[idx].second;
+        }
+    }
+    return res;
+}
+//https://leetcode.com/problems/find-peak-element/description/
+int findPeakElement(vector<int>& nums) {
+    int l = 0,r = nums.size()-1,ans = -1;
+    if(nums.size()==1)return 0;
+    while(l<r){
+        int mid = (l + r + 1)/2;
+        if(mid == 0 or nums[mid]>nums[mid-1])
+           l = mid;
+        else
+            r = mid-1;
+    }
+    return l;
+}
